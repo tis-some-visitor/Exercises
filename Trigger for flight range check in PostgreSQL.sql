5bd2d2c181c67d24aab7505197bfa2959a51dd04
tis-some-visitor <94824698+tis-some-visitor@users.noreply.github.com>
2022-01-24 18:08:49 +0300

    Update Trigger for flight range check in PostgreSQL.sql

--- Trigger for flight range check in PostgreSQL.sql
+++ Trigger for flight range check in PostgreSQL.sql
@@ -1,4 +1,5 @@
---"Создайте триггер, реализующий правило целостности в демонстрационной базе: рейсы могут совершать только те типы самолетов, максимальная дальность полета которых превышает расстояние между аэропортами"
+/* "Создайте триггер, реализующий правило целостности в демонстрационной базе: 
+рейсы могут совершать только те типы самолетов, максимальная дальность полета которых превышает расстояние между аэропортами" */
 
 create or replace function aircraft_range_to_flight_distance_check()
 returns trigger 
@@ -10,7 +11,7 @@ declare
 
 begin
 	select sec_to_gc(earth_distance(ll_to_earth(air.coord1[0], air.coord1[1]), ll_to_earth(air.coord2[0], air.coord2[1]))), air.f_range 
-    into dist, aircraft_range 
+    into distance, aircraft_range 
     from
         (select dep.coordinates as coord1, arr.coordinates as coord2, c.range as f_range 
         from flights f 
@@ -20,7 +21,7 @@ begin
         join aircrafts_data c on f.aircraft_code = c.aircraft_code 
         where f.flight_id = new.flight_id) as air;
 
-    if dist > cast(aircraft_range as float8) * 1000 then
+    if distance > cast(aircraft_range as float8) * 1000 then
     raise exception 'Aircraft % has shorter flight range than needed for this flight', new.aircraft_code;
 
     end if;
